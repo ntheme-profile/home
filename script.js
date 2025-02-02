853ed8a1f083b783510ab11538579fc6ba287916
ntheme-profile <bossnasir@gmail.com>
2025-02-02 23:45:56 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// This script could be used for smooth scrolling between sections.
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
